aedb930cdf0d222e0dc427c4e93f18f279c3f2b1
T0050646 <94188743+dom-astro@users.noreply.github.com>
2023-11-03 21:49:59 +0100

    Partie I
    Maj Lascaux + Stonehenge

--- js/lascaux.js
+++ js/lascaux.js
@@ -2,39 +2,63 @@ function addLascaux() {
 
     /* Introduction */
     $(".lascaux").append("\
+    <section style='text-align: center;'>\
+        <span style='font-size: var(--r-heading1-size); position: relative; top: -200px;'>Partie I</span><br><br>\
+        <span style='font-size: var(--r-heading2-size); position: relative; top: -200px;'>La préhistoire</span>\
+    </section>"
+    );
+
+    $(".lascaux").append("\
     <section class='lascaux-intro'>\
-        <span>L'astronomie est, parmi l'ensemble des sciences, celle qui est considérée comme la plus ancienne</span>\
-        <p class='fragment fade-up'><img src='img/nuage.png'>\
+        <span style='font-size: 50px;'>L'astronomie est, parmi l'ensemble des sciences, celle qui est considérée comme la plus ancienne.</span>\
+        <p class='fragment fade-up'><img src='img/nuage.png'></p>\
     </section>"
     );
 
     /* Cycles astronomiques */
     $(".lascaux").append("\
     <section class='lascaux-cycles'>\
-        L'humanité, en regardant le ciel, s'est petit à petit aperçu que plusieurs cycles régissaient le passage du temps\
+        L'humanité, en regardant le ciel, s'est petit à petit aperçu que plusieurs cycles régissaient le passage du temps.\
         <p class='fragment fade-up lascaux-cycles-stellarium'><span>Je vous propose de les découvrire à travers le site <a href='https://stellarium-web.org/' target='_blank'>stellarium-web.org</a><br>\
-        en faisant défiler une année entière jour par jour à 8h00 du matin</span></p>\
-        <p class='fragment fade-right lascaux-cycles-soleil'><img src='img/jour-nuit.gif'><span>L'alternance jour / nuit</span></p>\
-        <p class='fragment fade-left lascaux-cycles-lune'><span>Le cycle lunaire</span><img src='img/lune.gif' width='150px'></p>\
-        <p class='fragment fade-right lascaux-cycles-saisons'><video muted data-autoplay src='video/saisons.mp4'></video><span>La ronde des saisons</span></p>\
+        en faisant défiler une année entière jour par jour à 8h00 du matin.</span></p>\
+        <p class='fragment fade-right lascaux-cycles-soleil'><img src='img/jour-nuit.gif'><span>L'alternance jour / nuit.</span></p>\
+        <p class='fragment fade-left lascaux-cycles-lune'><span>Le cycle lunaire.</span><img src='img/lune.gif' width='150px'></p>\
+        <p class='fragment fade-right lascaux-cycles-saisons'><video muted data-autoplay src='video/saisons.mp4'></video><span>La ronde des saisons.</span></p>\
     </section>"
     );
 
     // La grotte de Lascaux
     $(".lascaux").append("\
-    <section>\
-        La grotte de Lascaux, située dans la vallée de la Vézère en Dordogne, est l’une des plus importantes grottes ornées du Paléolithique supérieur\
-        par le nombre et la qualité esthétique de ses peintures et gravures, principalement des représentations d’animaux.\
-        Les peintures rupestres de Lascaux témoignent d’une longue tradition d’observation du ciel.\
-        Les scientifiques ont découvert que les peintures rupestres représentent des constellations telles que le Taureau, Orion et les Pléiades.\
-        Les hommes préhistoriques utilisaient probablement ces constellations pour suivre le temps et les saisons en observant les mouvements des étoiles.\
+    <section class='lascaux-peintures'>\
+        <span style='font-size: var(--r-heading3-size);'>La grotte de Lascaux </span>\
+        <span style='font-size: 30px;'>(entre -20.000 et -18.000 ans av. J.-C.)</span>\
+        <p class='fragment fade-down lascaux-peintures-taureau'>\
+            <img src='img/Lascaux-Taureau.jpg' class='rounded border border-danger-subtle' alt='Lascaux'>\
+            <span>L'une des toutes premières représentations du ciel se trouve dans la grotte de Lascaux\
+            située dans la vallée de la Vézère en Dordogne.<br>\
+            D'après les scientifiques, certaines peintures rupestres\
+            représentent des constellations telles que le Taureau, Orion et les Pléiades.</span>\
+            <span class='fig-caption'>Crédit : Ministère de la Culture/Centre National de la Préhistoire</span>\
+        </p>\
+        <p class='fragment fade-up lascaux-peintures-end'>\
+            <span>Faisons la comparaison avec la constellation du taureau à travers le site <a href='https://stellarium-web.org/' target='_blank'>stellarium-web.org</a>\
+            </span>\
+        </p>\
     </section>"
     );
 
+    // Stonehenge
     $(".lascaux").append("\
     <section>\
-        C’est ensuite au Néolithique que de nouvelles preuves de l’intérêt de l’Homme pour l’étude du ciel apparaissent avec les mégalithes. Ces monuments constitués d’une ou plusieurs pierres de grande taille érigées sans mortier ni ciment auraient un rôle multiple : social et culturel, mais aussi astronomique.\
+        <span style='font-size: var(--r-heading3-size);'>Les mégalithes de Stonehenge </span>\
+        <span style='font-size: 30px;'>(entre -3.500 et -3.000 ans av. J.-C.)</span>\
+        <p class='fragment fade-down lascaux-stonehenge'>\
+            <img src='img/stonehenge.jpg' class='rounded border border-danger-subtle' alt='Stonehedge'>\
+            <span>Cette architecture est représentative de l'architecture de certains monuments mégalithiques qui\
+            répondrait à des critères d'orientation astronomique, leur permettant d'être utilisés comme calendriers solaires (solstice, équinoxe)\
+            ou pour la prévision d'évènements astraux réguliers.</span>\
+            <span class='fig-caption'>Soltice d'été à Stonehedge</span>\
+        </p>\
     </section>"
     );
-
 }
